3fab7895a47125ed747e4ccaa8e8e958d6aa7773
1PhanTrungKien <117971378+1PhanTrungKien@users.noreply.github.com>
2023-05-26 14:40:47 +0700

    create cart

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\Users\LoginController;
-use App\Http\Controllers\Admin\MainController;
-use App\Http\Controllers\Admin\MenuController;
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\SliderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,30 +13,16 @@ use App\Http\Controllers\Admin\SliderController;
 |
 */
 
-Route::get('login', [LoginController::class, 'index'])->name('login');
-Route::post('admin/users/login/store', [LoginController::class, 'store']);5
-
-Route::middleware(['auth'])->group(function () {
-
-    Route::prefix('admin')->group(function () {
-
-        Route::get('/', [MainController::class, 'index'])->name('admin');
-        Route::get('main', [MainController::class, 'index']);
-
-   
-
-        #Product
-        Route::prefix('products')->group(function () {
-            Route::get('add', [ProductController::class, 'create']);
-            Route::post('add', [ProductController::class, 'store']);
-            Route::get('list', [ProductController::class, 'index']);
-            Route::get('edit/{product}', [ProductController::class, 'show']);
-            Route::post('edit/{product}', [ProductController::class, 'update']);
-            Route::DELETE('destroy', [ProductController::class, 'destroy']);
-        });
-
-       
-       
+Route::get('/', function () {
+    return view('welcome');
 });
 
- 
+ #Product
+ Route::prefix('products')->group(function () {
+    Route::get('add', [ProductController::class, 'create']);
+    Route::post('add', [ProductController::class, 'store']);
+    Route::get('list', [ProductController::class, 'index']);
+    Route::get('edit/{product}', [ProductController::class, 'show']);
+    Route::post('edit/{product}', [ProductController::class, 'update']);
+    Route::DELETE('destroy', [ProductController::class, 'destroy']);
+});
