2b13024da22daf1469eb650cc208fe43981f2794
PhuongLoc <103478293+PhuongLoc@users.noreply.github.com>
2023-05-26 14:38:05 +0700

    cm1

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,25 @@ use App\Http\Controllers\Admin\SliderController;
 */
 
 Route::get('login', [LoginController::class, 'index'])->name('login');
-Route::post('admin/users/login/store', [LoginController::class, 'store']);5
+Route::post('admin/users/login/store', [LoginController::class, 'store']); 
 
 Route::middleware(['auth'])->group(function () {
 
     Route::prefix('admin')->group(function () {
 
+
         Route::get('/', [MainController::class, 'index'])->name('admin');
         Route::get('main', [MainController::class, 'index']);
 
-   
+       #Menu
+       Route::prefix('menus')->group(function () {
+        Route::get('add', [MenuController::class, 'create']);
+        Route::post('add', [MenuController::class, 'store']);
+        Route::get('list', [MenuController::class, 'index']);
+        Route::get('edit/{menu}', [MenuController::class, 'show']);
+        Route::post('edit/{menu}', [MenuController::class, 'update']);
+        Route::DELETE('destroy', [MenuController::class, 'destroy']);
+    });
 
         #Product
         Route::prefix('products')->group(function () {
@@ -44,4 +53,4 @@ Route::middleware(['auth'])->group(function () {
        
 });
 
- 
+});
